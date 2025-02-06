805d97abb148b055815a3db7d8722ea906236f65
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-02-06 20:30:40 +0400

    Revert "Preview (#1)"
    This reverts commit c08013fe91a0a528c97f843fa2434696a76776e9.

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("Navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
