6d8e020a7581ab7d8bd447d82a1b77aa95900da4
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-04-01 13:43:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,5 @@ function myFunction() {
 }
 
 function promptAddToHomeScreen() {
-  alert('To add this app to your Home Screen:\n1. Go to the home page (clicking on "Close" will do so).\n2. Click on the share button.\n3. Select "Add to Home Screen."');
-  window.location.href = '/'; // Adjust the path to your home page as needed
+  alert('To add this app to your Home Screen:.\n1. Click on the share button.\n2. Select "Add to Home Screen."');
 }
