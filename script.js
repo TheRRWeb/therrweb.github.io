0b3ddbe504c5e7264ad3eb2c76708a43fe22e05b
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-04-01 13:50:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ function myFunction() {
 }
 
 function promptAddToHomeScreen() {
-  alert('To add this app to your Home Screen:.\n1. Click on the share button.\n2. Select "Add to Home Screen."');
+  alert('Make it EASY to open the web by getting the app!\n\nTo add this app to your Home Screen:.\n1. Click on the share button.\n2. Select "Add to Home Screen."');
 }
