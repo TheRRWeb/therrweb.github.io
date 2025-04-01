05d7f9d407c32820631f51a5bb02f217f7c02fae
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-04-01 11:15:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+function promptAddToHomeScreen() {
+  alert('To add this app to your Home Screen:\n1. Go to the home page (clicking on "Close" will do so).\n2. Click on the share button.\n3. Select "Add to Home Screen."');
+  window.location.href = '/index.html'; // Adjust the path to your home page as needed
+}
