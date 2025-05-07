af09e3523de16f5c79f5965a697f1572dcd77859
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-05-07 18:28:58 +0400

    Update gamesjs.js

--- gamesjs.js
+++ gamesjs.js
@@ -1,9 +1,4 @@
 // Function to open the game in fullscreen or the desired format
-// Assuming you have the user's name after Google sign-in
-const userName = 'John Doe'; // Replace this with the actual user name obtained from Google
-
-// Call umami.identify with the user's name
-umami.identify({ name: userName });
 
 
 function openFullscreen(url) {
