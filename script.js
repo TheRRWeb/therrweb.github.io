913f59cd6bc30237855f636e5174b1ed6feadc77
Rutham Renish <142883347+RuthPro2012@users.noreply.github.com>
2025-05-16 20:52:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,3 @@ function myFunction() {
     x.className = "navbar";
   }
 }
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDnLY_bS5z2yG6kwfES597fUq_8n5rl5HU",
-  authDomain: "the-rr-web.firebaseapp.com",
-  projectId: "the-rr-web",
-  storageBucket: "the-rr-web.appspot.com",
-  messagingSenderId: "164968568706",
-  appId: "1:164968568706:web:1de6a6e34ab866bacd219b"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
